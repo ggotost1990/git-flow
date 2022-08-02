19dc987a4c79cfc898febe136fc0a50454a1e3ec
ggotost@gmail.com <ggotost@gmail.com>
2022-08-02 11:23:29 +0800

    print hello world

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
+<<<<<<< Updated upstream
 	fmt.Println("call main function")
+	fmt.Println("Hello World")
+=======
+	fmt.Println("callmainfunction")
+	fmt.Println("Hello")
+>>>>>>> Stashed changes
 }
